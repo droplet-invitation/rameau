230c58297e30a4bc905dd15d38a95df599120a3c
droplet-invitation <ohseoyeon@ohseoyeons-MacBook-Pro.local>
2022-04-18 21:18:06 +0900

    top 고정바 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import TopNavBar from './components/TopNavBar';
+
 
 function App() {
   return (
     <div className="App">
+      <TopNavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/TopNavBar/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import media from '../../shared/mediaQuery';
+import { css } from '@emotion/css';
+
+const topNavBar = css`
+  height: 60px;
+  line-height: 60px;
+  background-color: rgb(58, 64, 42);
+  color: #ffffff;
+`;
+
+const TopNavBar = () => {
+  return (
+    <div className={topNavBar}>
+      박유진 ♥ 장동선 결혼합니다.
+    </div>
+  );
+}
+
+export default TopNavBar
